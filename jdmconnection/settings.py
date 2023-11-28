61f1b789b13fac6f86cc5a3d00214ce57f59caf7
nicolasOyarce <101960895+nicolasOyarce@users.noreply.github.com>
2023-11-28 10:21:33 -0300

    change to sqlite3 bd

--- jdmconnection/settings.py
+++ jdmconnection/settings.py
@@ -82,13 +82,10 @@ WSGI_APPLICATION = 'jdmconnection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jdmconnection',
-        'USER': 'nico',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '5432',
-    }}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -109,11 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
 
 TIME_ZONE = 'UTC'
 
