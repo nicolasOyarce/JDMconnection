2d795e5c72855d64fbe163be478408f0e8716956
Alekplay23 <50862174+Alekplay23@users.noreply.github.com>
2023-11-28 01:25:03 -0300

    update item shopping_cart

--- cars/cart.py
+++ cars/cart.py
@@ -1,3 +1,4 @@
+from .models import Cars
 class ShoppingCart:
 
     def __init__(self, request):
@@ -13,31 +14,35 @@ class ShoppingCart:
 
     
 
-    def add(self, product):
-        product_id = str(product.id)
+    def add(self, product, quantity=1):
+            """
+            Add a product to the cart or update its quantity.
+            """
+            product_id = str(product.id)
 
-        if product_id not in self.cart.keys():
-            self.cart[product_id] = {
-                "car_id": product.id,
-                "brand": product.brand,
-                "model": product.model,
-                "stock": product.stock,
-                "quantity": 1,
-                "age": product.age,
-                "price": str(product.price),
-                "image": product.image.url,
-            }
-        else:
-            for key, value in self.cart.items():
-                if key == product_id:
-                    if value["quantity"] + 1 > product.stock:
-                        return  
+            if product_id not in self.cart.keys():
+                if quantity > product.stock:
+                    return "Estás superando el stock disponible"
+                self.cart[product_id] = {
+                    "car_id": product.id,
+                    "brand": product.brand,
+                    "model": product.model,
+                    "stock": product.stock,
+                    "quantity": quantity,  # Set the initial quantity to the provided quantity
+                    "age": product.age,
+                    "price": str(product.price * quantity),  # Multiply the price by the quantity
+                    "image": product.image.url,
+                }
+            else:
+                for key, value in self.cart.items():
+                    if key == product_id:
+                        if value["quantity"] + quantity > product.stock:
+                            return "Estás superando el stock disponible"
+                        value["quantity"] = value["quantity"] + quantity
+                        value["price"] = int(value["price"]) + product.price * quantity  # Multiply the price by the quantity
+                        break
 
-                    value["quantity"] = value["quantity"] + 1
-                    value["price"] = int(value["price"]) + product.price
-                    break
-
-        self.save()
+            self.save()
 
 
         
@@ -67,5 +72,12 @@ class ShoppingCart:
 
 
     def clear(self):
+        for key, value in self.cart.items():
+            product = Cars.objects.get(id=key)
+            if product.stock >= value["quantity"]:
+                product.stock -= value["quantity"]
+                product.save()
+            else:
+                return "No hay suficiente stock para completar la compra"
         self.session["cart"] = {}
         self.session.modified = True
--- jdmconnection/settings.py
+++ jdmconnection/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'jdmconnection',
-        'USER': 'nico',
-        'PASSWORD': '1234',
+        'USER': 'alekplay',
+        'PASSWORD': '2003',
         'HOST': '127.0.0.1',
         'DATABASE_PORT': '5432',
     }}
@@ -139,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CART_SESSION_ID = 'cart'
